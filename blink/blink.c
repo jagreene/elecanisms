7333c75392da88f5d5202a788de6fd584fc9423e
jagreene <austin.greene@students.olin.edu>
2016-01-21 13:36:41 -0500

    updated scons file and blink

--- blink/blink.c
+++ blink/blink.c
@@ -5,12 +5,17 @@
 #include "timer.h"
 
 int16_t main(void) {
+    int led2_on;
+    int led3_on;
+
+    led2_on = 0;
+    led3_on = 1;
     init_clock();
     init_ui();
     init_timer();
 
     led_on(&led1);
-    timer_setPeriod(&timer2, 0.5);
+    timer_setPeriod(&timer2, 0.1);
     timer_start(&timer2);
 
     while (1) {
@@ -18,8 +23,20 @@ int16_t main(void) {
             timer_lower(&timer2);
             led_toggle(&led1);
         }
-        led_write(&led2, !sw_read(&sw2));
-        led_write(&led3, !sw_read(&sw3));
+
+        if(!sw_read(&sw2)){
+            timer_setPeriod(&timer2, 0.5);
+            timer_start(&timer2);
+            led2_on = 1;
+            led3_on = 0;
+        } else if(!sw_read(&sw3)){
+            timer_setPeriod(&timer2, 0.1);
+            timer_start(&timer2);
+            led2_on = 0;
+            led3_on = 1;
+        }
+        led_write(&led2, led2_on);
+        led_write(&led3, led3_on);
     }
 }
 
